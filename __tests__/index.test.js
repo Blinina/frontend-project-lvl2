84bed8625de5313c5ca7fbba6aaf79a31b0562c9
blinina <anastasia.paancake@gmail.com>
2022-06-26 19:54:48 +0300

    step 3

---
+++ __tests__/index.test.js
@@ -0,0 +1,10 @@
+import genDiff from '../src/index.js';
+import fs from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import fs from 'fs'
-import {resolve}  from 'path'
-import genDifferents from '../src/index.js'
+import path  from 'path'
+import genDifferents from '../src/getdiff.js'
 
 const genDiff = (filepath1, filepath2) =>{
 
-    const absolutPath1 = resolve(filepath1);
-    const absolutPath2 = resolve(filepath2);
+    const absolutPath1 = path.resolve(process.cwd(), '__fixtures__,', filepath1);
+    const absolutPath2 = path.resolve(process.cwd(), '__fixtures__,', filepath2);
 
 
     const contentFile1  = fs.readFileSync(absolutPath1, 'utf8');
     const contentFile2  = fs.readFileSync(absolutPath2, 'utf8');
 
-    const date1 = JSON.parse(contentFile1);
-    const date2 = JSON.parse(contentFile2);
+    const data1 = JSON.parse(contentFile1);
+    const data2 = JSON.parse(contentFile2);
 
-    const result = genDifferents(date1, date2);
+    const result = genDifferents(data1, data2);
+    console.log(result);
+
     return result;
-    
 
 }
 export default genDiff;
